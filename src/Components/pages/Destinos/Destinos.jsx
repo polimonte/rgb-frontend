000266e606c255f42da239bc28e1f769a5d61ba3
polimonte <polianedomonte@gmail.com>
2022-03-24 23:03:46 -0300

    fix-loading

--- src/Components/pages/Destinos/Destinos.jsx
+++ src/Components/pages/Destinos/Destinos.jsx
@@ -1,14 +1,14 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import './Destinos.css'
-//import { FormDestinos } from "../../componentes/forms/Form";
+import { FormDestinos } from "../../componentes/forms/Form";
 
-const FormDestinos = lazy(() => import('../../componentes/forms/Form'));
+//const FormDestinos = lazy(() => import('../../componentes/forms/Form'));
 
 export default function Destinos() {
     return (
-        <Suspense fallback={<h2>Carregando....</h2>}>
+        
             <FormDestinos />
-        </Suspense>
+        
 
     );
 }
