36ccf0137d8abcff7fc14034b2971eabd9e09901
polimonte <polianedomonte@gmail.com>
2022-03-24 22:42:31 -0300

    link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,12 +9,10 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-black" id="menu">
             <div className="container-fluid">
                 <Link to="/">
-                    <a href="#" className="navbar-brand">
-                        <ImgNavBar
-                            src={Logo}
-                            alt="RGB Viagens"
-                        />
-                    </a>
+                    <ImgNavBar
+                        src={Logo}
+                        alt="RGB Viagens"
+                    />
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -22,28 +20,28 @@ export default function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link to="/">
-                                <a href="#" className="nav-link home" id="home" >HOME.</a>
+                            <Link to="/" className="nav-link home" id="home">
+                                HOME.
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/Destinos">
-                                <a href="#" className="nav-link destinos" id="destinos" >DESTINOS.</a>
+                            <Link to="/Destinos" className="nav-link destinos" id="destinos">
+                                DESTINOS.
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/Promocoes">
-                                <a href="#" className="nav-link promocoes" id="promocoes" >PROMOÇÕES.</a>
+                            <Link to="/Promocoes" className="nav-link promocoes" id="promocoes">
+                                PROMOÇÕES.
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/Contato">
-                                <a href="#" className="nav-link contatos" id="contatos">CONTATO.</a>
+                            <Link to="/Contato" className="nav-link contatos" id="contatos">
+                                CONTATO.
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/MeuCarrinho">
-                                <a href="#" className="nav-link meusDestinos" id="meusDestinos" >MEU CARRINHO.</a>
+                            <Link to="/MeuCarrinho" className="nav-link meusDestinos" id="meusDestinos">
+                                MEU CARRINHO.
                             </Link>
                         </li>
                     </ul>
